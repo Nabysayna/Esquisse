8166cbabc3cece13295800454f3498228f5253ec
Nabysayna <nabysayna@gmail.com>
2017-04-20 12:25:37 +0000

    third

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AuthComponentComponent } from './auth-component/auth-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponentComponent
   ],
   imports: [
     BrowserModule,
