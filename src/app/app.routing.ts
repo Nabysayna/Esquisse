86a9f9d21ec063ed8404c7d88c9601d484820bb7
Nabysayna <nabysayna@gmail.com>
2017-04-21 15:10:14 +0000

    routeapicmpnt

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,28 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponentComponent } from './auth-component/auth-component.component';
-import { AccueilComponent } from './accueil/accueil.component';
+import { AccueilComponent } from './accueil/accueil.component'; 
 import { RyaComponentComponent } from './rya-component/rya-component.component';
+import { JoniJoniComponentComponent } from './joni-joni-component/joni-joni-component.component';
+import { MoneyGramComponentComponent } from './money-gram-component/money-gram-component.component';
+import { OrangeMoneyComponentComponent } from './orange-money-component/orange-money-component.component';
+import { PostCashComponentComponent } from './post-cash-component/post-cash-component.component';
+import { TigoCashComponentComponent } from './tigo-cash-component/tigo-cash-component.component';
+import { WesternUnionComponentComponent } from './western-union-component/western-union-component.component';
 
 const appRoutes: Routes = [
     { path: '', component: AuthComponentComponent },
     { path: 'accueil', component: AccueilComponent, 
-    children:[{path: 'rya', component: RyaComponentComponent}] 
+    children:[
+    			{path: 'RYA', component: RyaComponentComponent},
+    			{path: 'JONI JONI', component: JoniJoniComponentComponent},
+    			{path: 'MONEYGRAM', component: MoneyGramComponentComponent},
+    			{path: 'ORANGE MONEY', component: OrangeMoneyComponentComponent},
+    			{path: 'POSTECASH', component: PostCashComponentComponent},    			
+    			{path: 'TIGO CASH', component: TigoCashComponentComponent},
+    			{path: 'WESTERN UNION', component: WesternUnionComponentComponent},
+
+    		] 
     },
 
     { path: 'accueil', component: AccueilComponent },
