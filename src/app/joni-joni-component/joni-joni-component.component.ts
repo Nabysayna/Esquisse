708593fe36f3c421d0e7c9eb31cdba57f576bef0
gningbamba <kha8224@gmail.com>
2017-04-25 11:28:02 +0000

    g

--- src/app/joni-joni-component/joni-joni-component.component.ts
+++ src/app/joni-joni-component/joni-joni-component.component.ts
@@ -8,17 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class JoniJoniComponentComponent implements OnInit {
 
  formvisible = true ;
-  pn : string ;
-  tel : string ; 
-  pnd : string ;
-  teld : string ;
-  mnt : number ;
+    nome : string ;
+    prenome : string ;
+    tele : string ;
+    nomd : string ;
+    prenomd : string ;
+    teld : string ;
+    mnt : number ;
+    nomp : string ;
+    tel : string;
+    ref : string;
+    mntf: string;
+    mntr: string;
+    telr : string;
+    mntrc:string;
+    numc:string;
 
-  constructor() { }
+    constructor() { }
   
-  validform(){}
+    validform(){}
 
-  ngOnInit() {
+    paiementform(){}
+
+    rechargevtfform(){}
+
+    rechargecjjform(){}
+
+    ngOnInit() {
   }
 
 }
