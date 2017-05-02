7bcf28f86817e185d49dd97fed515bda73502ad4
gningbamba <kha8224@gmail.com>
2017-05-02 11:01:41 +0000

    g

--- src/app/soldecompte/soldecompte.component.ts
+++ src/app/soldecompte/soldecompte.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-soldecompte',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SoldecompteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
