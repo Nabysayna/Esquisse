7bdac34efc5684ed49ae133e5de80fc11f52aa01
Nabysayna <nabysayna@gmail.com>
2017-05-31 10:13:23 +0000

    E-com improvement

--- src/app/espace-perso/espace-perso.component.ts
+++ src/app/espace-perso/espace-perso.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-
 export class Article {
 	public nomImg:string;
 	public designation:string;
-	public prix:number;
-	
+	public prix:number;	
 }
 
 @Component({
@@ -15,21 +13,15 @@ export class Article {
 })
 export class EspacePersoComponent implements OnInit {
 
-	articles:Article[][] =[[{nomImg:"n", designation:"Chaussures homme", prix:18000},{nomImg:"nf", designation:"Chaussures femme", prix:15000},{nomImg:"hs", designation:"soulier homme", prix:20000}],
-   [{nomImg:"bha", designation:"basket homme", prix:13000},{nomImg:"bbb", designation:"chaussures enfant", prix:8000},{nomImg:"mc", designation:"montre couple", prix:21000}]];
-
-	img : string [] = ['n','nf', 'bb', 'hs', 'fs', 'es','bha','bfa','bea','bbb','mc'] ;
-	desig : string [] = ['Chaussures homme','Chaussures femme', 'Chaussures enfant', 'soulier homme', 'soulier enfant', 'soulier femme','basket homme','basket femme','basket enfant','chaussures bb','montre couple'] ;
-	prix : string [] = ['18000','15000', '10000', '20000', '12000', '18000','13000','11000','9000','8000','21000'] ;
-
+	articles:Article[][] =[[{nomImg:"n", designation:"Chaussures homme", prix:18000},{nomImg:"nf", designation:"Chaussures femme", prix:15000},{nomImg:"hs", designation:"soulier homme", prix:20000},{nomImg:"bha", designation:"basket homme", prix:13000}, {nomImg:"bbb", designation:"chaussures enfant", prix:8000}],
+   [{nomImg:"mc", designation:"montre couple", prix:21000}]];
 
   constructor() { }
 
   ngOnInit() {
   }
 
- deleteArticle(article:Article) {
-      
+ deleteArticle(article:Article) {      
       for(var j=0; j<this.articles.length; j++){
       	var ligne=this.articles[j];
       		for (var i=0; i<ligne.length; i++)
@@ -41,7 +33,7 @@ export class EspacePersoComponent implements OnInit {
       			}
       }
 
-}
+  }
  
 
  ajouter(){
