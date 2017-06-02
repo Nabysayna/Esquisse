0fb8d0b2d4d63d4ba73389bb6bf145cdaca98be2
gningbamba <kha8224@gmail.com>
2017-06-02 12:08:03 +0000

    g

--- src/app/webServiceClients/Hello/hello.service.ts
+++ src/app/webServiceClients/Hello/hello.service.ts
@@ -5,7 +5,7 @@ import {SoapService} from "../../soap.service";
 @Injectable()
 export class HelloService {
 
-  private servicePort:string = 'http://localhost' ; 
+  private servicePort:string = 'http://localhost:8888' ; 
   private servicePath:string = '/dev-bbsinvest-plateform/EsquisseBackEnd/web/app_dev.php/helloService?wsdl' ;
   private targetNamespace:string = 'urn:helloservicewsdl' ;
 
